fab7eca41e754e1e50323adc4c2e3480429f733d
Joshua Hanson <nibblebot@gmail.com>
2019-08-03 15:41:26 -0500

    initial babylon/rollup/babel/eslint/prettier install

---
+++ rollup.config.dev.js
@@ -0,0 +1,20 @@
+import resolve from "rollup-plugin-node-resolve"
+import commonjs from "rollup-plugin-commonjs"
+import babel from "rollup-plugin-babel"
+import serve from "rollup-plugin-serve"
+
+export default {
+	input: "src/index.js",
+	output: {
+		file: "dist/bundle.js",
+		format: "cjs"
+	},
+	plugins: [
+		resolve(),
+		commonjs(),
+		babel({
+			exclude: "node_modules/**" // only transpile our source code
+		}),
+		serve("dist")
+	]
+}

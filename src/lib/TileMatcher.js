fed35539c94899d9801295ce9053d307eb4f5963
Joshua Hanson <nibblebot@gmail.com>
2019-08-11 18:46:31 -0500

    refactor horizontal match pure TileMatcher

---
+++ src/lib/TileMatcher.js
@@ -0,0 +1,147 @@
+export default class TileMatcher {
+	constructor(numRows, numColumns) {
+		// this.initialRows = initialRows
+		this._columns = numColumns
+		this.grid = []
+		this.matches = []
+	}
+	getLastRow() {
+		return this.grid[this.grid.length - 1]
+	}
+	setRow(row, value) {
+		this.grid[row] = value
+	}
+	addRow(row) {
+		this.grid.push(row)
+	}
+	numRows() {
+		return this.grid.length
+	}
+	numColumns() {
+		return this._columns
+	}
+	setTile(row, col, tile) {
+		this.grid[row][col] = tile
+	}
+
+	isMatch(row, col) {
+		const horizMatch = this.isHorizontalMatch(row, col)
+		if (horizMatch) {
+			return true
+		}
+		// const vertMatch = this.isVerticalMatch(row, col)
+		// if (vertMatch) {
+		// 	return true
+		// }
+		return false
+	}
+
+	isHorizontalMatch(row, col) {
+		// return (
+		// 	this.grid[row][col].tileColor ==
+		// 		this.grid[row][col - 1].tileColor &&
+		// 	this.grid[row][col].tileColor == this.grid[row][col - 2].tileColor
+		// )
+		return (
+			this.tileAt(row, col).tileColor ==
+				this.tileAt(row, col - 1).tileColor &&
+			this.tileAt(row, col).tileColor ==
+				this.tileAt(row, col - 2).tileColor
+		)
+	}
+
+	isVerticalMatch(row, col) {
+		return (
+			this.tileAt(row, col).tileColor ==
+				this.tileAt(row - 1, col).tileColor &&
+			this.tileAt(row, col).tileColor ==
+				this.tileAt(row - 2, col).tileColor
+		)
+	}
+
+	tileAt(row, col) {
+		if (
+			row < 0 ||
+			row >= this.grid.length ||
+			col < 0 ||
+			col >= this.numColumns()
+		) {
+			return -1
+		}
+		return this.grid[row][col]
+	}
+
+	checkMatches() {
+		if (!this.hasMatches()) {
+			return false
+		}
+		this.resetMatches()
+		return this.markHorizontalMatches()
+	}
+
+	hasMatches() {
+		for (let i = 0; i < this.grid.length; i++) {
+			for (let j = 0; j < this.numColumns(); j++) {
+				if (this.isMatch(i, j)) {
+					return true
+				}
+			}
+		}
+	}
+
+	resetMatches() {
+		this.matches = []
+		for (let i = 0; i < this.grid.length; i++) {
+			this.matches[i] = []
+			for (let j = 0; j < this.numColumns(); j++) {
+				this.matches[i][j] = 0
+			}
+		}
+	}
+
+	markHorizontalMatches() {
+		let match = false
+		for (let i = 0; i < this.grid.length; i++) {
+			let colorStreak = 1
+			let colorToWatch = 0
+			let startStreak = 0
+			let currentColor = -2
+			for (let j = 0; j < this.numColumns(); j++) {
+				// watch for current tile color
+				colorToWatch = this.tileAt(i, j).tileColor
+
+				// skip empty tiles
+				if (colorToWatch < 0) {
+					continue
+				}
+
+				// extend streak if match
+				const colorMatch = colorToWatch === currentColor
+				if (colorMatch) {
+					colorStreak++
+				}
+				// if no match or on the edge
+				if (!colorMatch || j === this.numColumns() - 1) {
+					// ... and we already have a match
+					if (colorStreak >= 3) {
+						console.log("streak: ", colorStreak)
+						match = true
+						for (let k = 0; k < colorStreak; k++) {
+							this.matches[i][startStreak + k]++
+						}
+					}
+					startStreak = j
+					colorStreak = 1
+					currentColor = colorToWatch
+				}
+			}
+		}
+		return match
+	}
+
+	hasMatch(row, col) {
+		return this.matches[row][col]
+	}
+
+	checkMatchingNeighbors() {}
+}
--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,5 +1,6 @@
 import last from "ramda/src/last"
 import head from "ramda/src/head"
+import TileMatcher from "../lib/TileMatcher"
 
 const options = {
 	tileSize: 45,
@@ -13,9 +14,6 @@ const options = {
 	startingRows: 5,
 	destroySpeed: 4000
 }
-
-const HORIZONTAL = 1
-const VERTICAL = 2
 
 export default class GameScene extends Phaser.Scene {
 	constructor() {
@@ -69,6 +67,8 @@ export default class GameScene extends Phaser.Scene {
 		// Group of tiles with physics
 		this.tileGroup = this.physics.add.group()
 
+		this.matcher = new TileMatcher(options.startingRows, options.numColumns)
+
 		// create sprites and store matrix of tile data
 		this.initializeGameMatrix()
 		this.renderDebug()
@@ -76,10 +76,9 @@ export default class GameScene extends Phaser.Scene {
 	}
 
 	initializeGameMatrix() {
-		this.gameMatrix = []
 		for (let i = 0; i < options.startingRows; i++) {
-			this.gameMatrix[i] = []
-			for (let j = 0; j < options.numColumns; j++) {
+			this.matcher.setRow(i, [])
+			for (let j = 0; j < this.matcher.numColumns(); j++) {
 				// create tile
 				// const rowOffset = this.gameMatrix.length - 1 - i
 				const tile = this.createTile(
@@ -94,13 +93,13 @@ export default class GameScene extends Phaser.Scene {
 						Math.random() * options.numTiles
 					)
 					tile.setFrame(randomColor)
-					this.gameMatrix[i][j] = {
+					this.matcher.setTile(i, j, {
 						tileColor: randomColor,
 						tileSprite: tile,
 						isActive: true,
 						isEmpty: false
-					}
-				} while (this.isMatch(i, j))
+					})
+				} while (this.matcher.isMatch(i, j))
 			}
 		}
 	}
@@ -120,16 +119,16 @@ export default class GameScene extends Phaser.Scene {
 
 		// render debug grid
 		// for (let row = 0; row < this.gameMatrix.length; row++) {
-		for (let row = this.gameMatrix.length - 1; row >= 0; row--) {
-			for (let col = 0; col < options.numColumns; col++) {
+		for (let row = this.matcher.numRows() - 1; row >= 0; row--) {
+			for (let col = 0; col < this.matcher.numColumns(); col++) {
 				let sprite
 				const x = this.frameWidth + 6 + options.tileSize * col
 				const y =
 					this.game.config.height -
 					56 -
 					// options.tileSize * row
-					options.tileSize * (this.gameMatrix.length - 1 - row)
-				let text = this.gameMatrix[row][col].tileColor
+					options.tileSize * (this.matcher.numRows() - 1 - row)
+				let text = this.matcher.tileAt(row, col).tileColor
 				if (text === -1) {
 					text = "-"
 				}
@@ -164,204 +163,77 @@ export default class GameScene extends Phaser.Scene {
 		return tile
 	}
 
-	isMatch(row, col) {
-		const horizMatch = this.isHorizontalMatch(row, col)
-		if (horizMatch) {
-			return true
-		}
-		const vertMatch = this.isVerticalMatch(row, col)
-		if (vertMatch) {
-			return true
-		}
-		return false
-	}
-
-	tileAt(row, col) {
-		if (
-			row < 0 ||
-			row >= this.gameMatrix.length ||
-			col < 0 ||
-			col >= options.numColumns
-		) {
-			return -1
-		}
-		return this.gameMatrix[row][col]
-	}
-
-	isHorizontalMatch(row, col) {
-		return (
-			this.tileAt(row, col).tileColor ==
-				this.tileAt(row, col - 1).tileColor &&
-			this.tileAt(row, col).tileColor ==
-				this.tileAt(row, col - 2).tileColor
-		)
-	}
-
-	isVerticalMatch(row, col) {
-		return (
-			this.tileAt(row, col).tileColor ==
-				this.tileAt(row - 1, col).tileColor &&
-			this.tileAt(row, col).tileColor ==
-				this.tileAt(row - 2, col).tileColor
-		)
-	}
-
 	pause() {
 		this.scene.pause()
 		this.scene.run("pause")
 	}
 
 	update() {
-		const lastTileY = last(this.tileGroup.getChildren()).y
+		const lastRow = this.matcher.getLastRow()
+		const lastTileY = lastRow[0].tileSprite.y
 		const isLastRowAboveFrame = this.originY - lastTileY > 0
 		if (isLastRowAboveFrame) {
 			this.renderDebug()
 			this.activateLastRow()
-			if (this.boardHasMatches()) {
-				this.resetMatches()
-				this.markMatches(HORIZONTAL)
-				this.markMatches(VERTICAL)
+			if (this.matcher.checkMatches()) {
 				this.freezeTiles()
 				this.destroyMarkedTiles()
 			}
+			// if (this.boardHasMatches()) {
+			// 	this.resetMatches()
+			// 	this.markHorizontalMatches()
+			// }
 			this.addInactiveGameRow()
 		}
 	}
 
 	activateLastRow() {
-		last(this.gameMatrix).forEach(item => {
+		this.matcher.getLastRow().forEach(item => {
 			item.isActive = true
-		})
-		// TODO: refactor to only iterate last row
-		this.tileGroup.getChildren().forEach(tile => {
-			if (tile.isTinted) {
-				tile.clearTint()
+			if (item.tileSprite.isTinted) {
+				item.tileSprite.clearTint()
 			}
 		})
-	}
-
-	boardHasMatches() {
-		for (let row = 0; row < this.gameMatrix.length; row++) {
-			for (let col = 0; col < options.numColumns; col++) {
-				if (this.isMatch(row, col)) {
-					return true
-				}
-			}
-		}
-		return false
-	}
-
-	markMatches(direction) {
-		let match = false
-		let iMax
-		let jMax
-		// scan for horizontal with left->right inner loop
-		if (direction === HORIZONTAL) {
-			iMax = this.gameMatrix.length
-			jMax = options.numColumns
-		}
-		// scan for vertical with top->bottom inner loop
-		else {
-			iMax = options.numColumns
-			jMax = this.gameMatrix.length
-		}
-
-		for (let i = 0; i < iMax; i++) {
-			let colorStreak = 1
-			let colorToWatch = 0
-			let startStreak = 0
-			let currentColor = -2
-			for (let j = 0; j < jMax; j++) {
-				// watch for current tile color
-				const x = direction === HORIZONTAL ? j : i
-				const y = direction === HORIZONTAL ? i : j
-				colorToWatch = this.tileAt(y, x).tileColor
-
-				// extend streak if match
-				const colorMatch = colorToWatch === currentColor
-				if (colorMatch) {
-					colorStreak++
-				}
-				// if no match or on the edge
-				if (!colorMatch || j === jMax - 1) {
-					// ... and we already have a match
-					if (colorStreak >= 3) {
-						console.log(
-							direction === HORIZONTAL ? "HORIZONTAL" : "VERTICAL"
-						)
-						console.log("streak: ", colorStreak)
-						match = true
-						// console.log(colorStreak + " combo!")
-						for (let k = 0; k < colorStreak; k++) {
-							const row =
-								direction === HORIZONTAL ? i : startStreak + k
-							const col =
-								direction === HORIZONTAL ? startStreak + k : i
-
-							// console.log("matching: ", row, col)
-							this.matchMatrix[row][col]++
-						}
-					}
-					startStreak = j
-					colorStreak = 1
-					currentColor = colorToWatch
-				}
-			}
-		}
-		return match
 	}
 
 	freezeTiles() {
 		this.frozen = true
-		for (let row = 0; row < this.gameMatrix.length; row++) {
-			for (let col = 0; col < options.numColumns; col++) {
-				this.gameMatrix[row][col].tileSprite.setVelocity(0, 0)
+		for (let row = 0; row < this.matcher.numRows(); row++) {
+			for (let col = 0; col < this.matcher.numColumns(); col++) {
+				this.matcher.tileAt(row, col).tileSprite.setVelocity(0, 0)
 			}
 		}
 	}
 
 	unfreezeTiles() {
 		this.frozen = false
-		for (let row = 0; row < this.gameMatrix.length; row++) {
-			for (let col = 0; col < options.numColumns; col++) {
-				this.gameMatrix[row][col].tileSprite.setVelocity(
-					0,
-					options.tileDefaultSpeed
-				)
-			}
-		}
-	}
-
-	checkMatchingNeighbors() {}
-
-	resetMatches() {
-		this.matchMatrix = []
-		const rows = this.gameMatrix.length
-		const cols = this.gameMatrix[0].length
-		for (let i = 0; i < rows; i++) {
-			this.matchMatrix[i] = []
-			for (let j = 0; j < cols; j++) {
-				this.matchMatrix[i][j] = 0
+		for (let row = 0; row < this.matcher.numRows(); row++) {
+			for (let col = 0; col < this.matcher.numColumns(); col++) {
+				this.matcher
+					.tileAt(row, col)
+					.tileSprite.setVelocity(0, options.tileDefaultSpeed)
 			}
 		}
 	}
 
 	destroyMarkedTiles() {
 		let destroyed = 0
-		for (let i = 0; i < this.gameMatrix.length; i++) {
-			for (let j = 0; j < options.numColumns; j++) {
-				if (this.matchMatrix[i][j] > 0) {
+		for (let i = 0; i < this.matcher.numRows(); i++) {
+			for (let j = 0; j < this.matcher.numColumns(); j++) {
+				if (this.matcher.hasMatch(i, j) > 0) {
 					destroyed++
 					this.tweens.add({
-						targets: this.gameMatrix[i][j].tileSprite,
+						targets: this.matcher.tileAt(i, j).tileSprite,
 						alpha: 0.5,
 						duration: options.destroySpeed,
 						callbackScope: this,
 						onComplete: function() {
 							destroyed--
-							this.gameMatrix[i][j].tileSprite.visible = false
-							this.gameMatrix[i][j].isEmpty = true
-							this.tilePool.push(this.gameMatrix[i][j].tileSprite)
+
+							const tile = this.matcher.tileAt(i, j)
+							tile.tileSprite.visible = false
+							tile.isEmpty = true
+							this.tilePool.push(tile.tileSprite)
 							if (destroyed == 0) {
 								this.makeTilesFall()
 							}
@@ -376,9 +248,9 @@ export default class GameScene extends Phaser.Scene {
 		let currentTile
 		let falling = 0
 		let hasFalling = false
-		for (let i = this.gameMatrix.length - 2; i >= 0; i--) {
-			for (let j = 0; j < options.numColumns; j++) {
-				currentTile = this.gameMatrix[i][j]
+		for (let i = this.matcher.numRows() - 2; i >= 0; i--) {
+			for (let j = 0; j < this.matcher.numColumns(); j++) {
+				currentTile = this.matcher.tileAt(i, j)
 				if (!currentTile.isEmpty) {
 					let fallTiles = this.holesBelow(i, j)
 					if (fallTiles > 0) {
@@ -386,7 +258,8 @@ export default class GameScene extends Phaser.Scene {
 						falling++
 						this.tweens.add({
 							targets: currentTile.tileSprite,
-							y: this.gameMatrix[i + fallTiles][j].tileSprite.y,
+							y: this.matcher.tileAt(i + fallTiles, j).tileSprite
+								.y,
 							duration: options.tileFallSpeed * fallTiles,
 							callbackScope: this,
 							onComplete: function() {
@@ -397,11 +270,11 @@ export default class GameScene extends Phaser.Scene {
 								}
 							}
 						})
-						this.gameMatrix[i + fallTiles][j] = {
+						this.matcher.setTile(i + fallTiles, j, {
 							tileSprite: currentTile.tileSprite,
 							tileColor: currentTile.tileColor,
 							isEmpty: false
-						}
+						})
 						currentTile.isEmpty = true
 						currentTile.tileColor = -1
 					}
@@ -416,8 +289,8 @@ export default class GameScene extends Phaser.Scene {
 
 	holesBelow(row, col) {
 		let result = 0
-		for (let i = row + 1; i < this.gameMatrix.length; i++) {
-			if (this.gameMatrix[i][col].isEmpty) {
+		for (let i = row + 1; i < this.matcher.numRows(); i++) {
+			if (this.matcher.tileAt(i, col).isEmpty) {
 				result++
 			}
 		}
@@ -426,25 +299,25 @@ export default class GameScene extends Phaser.Scene {
 
 	addInactiveGameRow() {
 		const row = []
-		this.gameMatrix.push(row)
-		for (let j = 0; j < options.numColumns; j++) {
+		for (let j = 0; j < this.matcher.numColumns(); j++) {
 			// create tile
-			const tile = this.createTile(
+			const tileSprite = this.createTile(
 				this.originX + j * options.tileSize,
 				this.originY + options.tileSize - 1
 			)
 
 			// set random color to tile
-			const randomTile = Math.floor(Math.random() * options.numTiles)
-			tile.setFrame(randomTile)
+			const tileColor = Math.floor(Math.random() * options.numTiles)
+			tileSprite.setFrame(tileColor)
 			// set inactive tint
-			tile.setTint(0x888888)
+			tileSprite.setTint(0x888888)
 
 			row[j] = {
-				tileSprite: tile,
-				tileColor: randomTile,
+				tileSprite,
+				tileColor,
 				isActive: false
 			}
 		}
+		this.matcher.addRow(row)
 	}
 }
